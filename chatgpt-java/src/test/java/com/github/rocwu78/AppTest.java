cafabc9d855e00dd540917639b74c68f0bea194b
rocwu78 <freewill.wu@gmail.com>
2023-03-11 17:20:52 +1100

    Add files via upload

---
+++ chatgpt-java/src/test/java/com/github/rocwu78/AppTest.java
@@ -0,0 +1,20 @@
+package com.github.rocwu78;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
